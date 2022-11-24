d0f3c4d42326157a472f9d6fbcce85ae7532da3c
Carbon-42 <116390874+Carbon-42@users.noreply.github.com>
2022-11-24 13:28:20 -0700

    correct pokemonList syntax

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 //Array of Pokemon
-let pokemonList= ([
-    pokemon1 ({name: 'Charmander', height: 0.6, types: ['scale', 'fire']}),
-    pokemon2 ({name: 'Pikachu', height: 0.4, types: ['electric', 'yellow']}),
-    pokemon3 ({name: 'Seel', height: 1.1, types: ['water', 'wet']})
-])
+let pokemonList= [
+    {name: 'Charmander', height: 0.6, types: ['scale', 'fire']},
+    {name: 'Pikachu', height: 0.4, types: ['electric', 'yellow']},
+    {name: 'Seel', height: 1.1, types: ['water', 'wet']}
+]
