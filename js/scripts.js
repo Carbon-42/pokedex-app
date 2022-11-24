4e152a3f22a211fcfe212b124d3c80bb96071078
Carbon-42 <116390874+Carbon-42@users.noreply.github.com>
2022-11-24 13:09:25 -0700

    created pokemonList

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,6 @@
-alert("Hello World!");
-let favoriteFood="Sushi";
-document.write(favoriteFood);
+//Array of Pokemon
+let pokemonList= ([
+    pokemon1 ({name: 'Charmander', height: 0.6, types: ['scale', 'fire']}),
+    pokemon2 ({name: 'Pikachu', height: 0.4, types: ['electric', 'yellow']}),
+    pokemon3 ({name: 'Seel', height: 1.1, types: ['water', 'wet']})
+])
