0d781116548fe65d39619a693fb9941f9001a502
Carbon-42 <116390874+Carbon-42@users.noreply.github.com>
2022-11-24 21:11:50 -0700

    Update scripts.js
    added array loop

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,22 @@
 //Array of Pokemon
 let pokemonList= [
-    {name: 'Charmander', height: 0.6, types: ['scale', 'fire']},
-    {name: 'Pikachu', height: 0.4, types: ['electric', 'yellow']},
-    {name: 'Seel', height: 1.1, types: ['water', 'wet']}
+    {name: 'Charmander', 
+    height: 0.6, 
+    types: ['scale', 'fire']
+},
+    {name: 'Pikachu', 
+    height: 0.4, 
+    types: ['electric', 'yellow']
+},
+    {name: 'Seel', 
+    height: 1.1, 
+    types: ['water', 'wet']
+}
 ]
+ //Array Loop Display
+let exclamation=""
+for (let i = 0; i < pokemonList.length; i++) {
+    if (pokemonList[i].height > 1) {exclamation= " - Wow that's big!"}
+    else exclamation= ""
+    document.write(pokemonList[i].name + " (height: " + pokemonList[i].height + "m)" + exclamation); 
+}
