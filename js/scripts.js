c60745270423949310b391d56338f567a582d328
Carbon-42 <116390874+Carbon-42@users.noreply.github.com>
2022-11-30 22:18:59 -0700

    Update scripts.js
    add IIFE
    update forEach loop

--- js/scripts.js
+++ js/scripts.js
@@ -1,28 +1,45 @@
 //Array of Pokemon
+let pokemonRepository = (function() {
+
 let pokemonList= [
-    {
-        name: 'Charmander', 
-        height: 0.6, 
-        types: ['scale', 'fire']
-    },
+        {
+            name: 'Charmander', 
+            height: 0.6, 
+            types: ['scale', 'fire']
+        },
 
-    {
-        name: 'Pikachu', 
-        height: 0.4, 
-        types: ['electric', 'yellow']
-    },
+        {
+            name: 'Pikachu', 
+            height: 0.4, 
+            types: ['electric', 'yellow']
+        },
 
-    {
-        name: 'Seel', 
-        height: 1.1, 
-        types: ['water', 'wet']
+        {
+            name: 'Seel', 
+            height: 1.1, 
+            types: ['water', 'wet']
+        }
+    ]
+
+    function getAll() {
+        return pokemonList;
     }
-]
+
+    function add(pokemon) {
+        pokemonList.push(pokemon);
+    }
+
+    return {
+        getAll: getAll,
+        add: add,
+    };
+
+})()
 
  //Array Loop Display
 let exclamation=""
 
-pokemonList.forEach(function(pokemon) {
+pokemonRepository.getAll().forEach(function(pokemon) {
     if (pokemon.height > 1) {exclamation= " - Wow that's big!"}
     else exclamation= ""
     document.write(pokemon.name + " (height: " + pokemon.height + "m)" + exclamation + '<br>'); 
