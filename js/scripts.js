644dcd1a95789198bd1caa8fb9578fe574e1aae2
Carbon-42 <116390874+Carbon-42@users.noreply.github.com>
2022-12-09 14:40:02 -0700

    Update scripts.js
    -indentation in pokemonRepository
    -add 'addListItem' function
    -replace forEach loop function

--- js/scripts.js
+++ js/scripts.js
@@ -1,46 +1,66 @@
 //Array of Pokemon
 let pokemonRepository = (function() {
 
-let pokemonList= [
-        {
-            name: 'Charmander', 
-            height: 0.6, 
-            types: ['scale', 'fire']
-        },
+        let pokemonList= [
+                {
+                    name: 'Charmander', 
+                    height: 0.6, 
+                    types: ['scale', 'fire']
+                },
 
-        {
-            name: 'Pikachu', 
-            height: 0.4, 
-            types: ['electric', 'yellow']
-        },
+                {
+                    name: 'Pikachu', 
+                    height: 0.4, 
+                    types: ['electric', 'yellow']
+                },
 
-        {
-            name: 'Seel', 
-            height: 1.1, 
-            types: ['water', 'wet']
+                {
+                    name: 'Seel', 
+                    height: 1.1, 
+                    types: ['water', 'wet']
+                }
+            ]
+
+        function getAll() {
+            return pokemonList;
         }
-    ]
 
-    function getAll() {
-        return pokemonList;
-    }
+        function add(pokemon) {
+            pokemonList.push(pokemon);
+        }
 
-    function add(pokemon) {
-        pokemonList.push(pokemon);
-    }
+        function addListItem(pokemon) {
+            let someList= document.querySelector('.pokemon-list');
+            let listItem= document.createElement('li');
+            let button= document.createElement('button');
+            button.innerText= pokemon.name;
+            button.classList.add('poke-button');
+            listItem.appendChild(button);
+            someList.appendChild(listItem);
+            button.addEventListener('click', function (event) {
+                showDetails(pokemon)
+            });
+        }        
 
-    return {
-        getAll: getAll,
-        add: add,
-    };
+        function showDetails(pokemon) {
+            console.log(pokemon);
+        }
+
+        return {
+            getAll: getAll,
+            add: add,
+            addListItem: addListItem,
+        };
 
 })()
 
  //Array Loop Display
-let exclamation=""
-
 pokemonRepository.getAll().forEach(function(pokemon) {
-    if (pokemon.height > 1) {exclamation= " - Wow that's big!"}
-    else exclamation= ""
-    document.write(pokemon.name + " (height: " + pokemon.height + "m)" + exclamation + '<br>'); 
+    pokemonRepository.addListItem(pokemon);
+    // pokemonRepository.showDetails(pokemon);
 })
+
+
+// if (pokemon.height > 1) {exclamation= " - Wow that's big!"}
+// else exclamation= ""
+// document.write(pokemon.name + " (height: " + pokemon.height + "m)" + exclamation + '<br>'); 
