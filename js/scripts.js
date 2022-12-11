0a8628efbd2e64621c6673fb0e7fbade3b078bee
Carbon-42 <116390874+Carbon-42@users.noreply.github.com>
2022-12-11 13:58:38 -0700

    Update scripts.js
    change indentation

--- js/scripts.js
+++ js/scripts.js
@@ -1,56 +1,55 @@
 //Array of Pokemon
 let pokemonRepository = (function() {
+     let pokemonList= [
+            {
+                name: 'Charmander', 
+                height: 0.6, 
+                types: ['scale', 'fire']
+            },
 
-        let pokemonList= [
-                {
-                    name: 'Charmander', 
-                    height: 0.6, 
-                    types: ['scale', 'fire']
-                },
+            {
+                name: 'Pikachu', 
+                height: 0.4, 
+                types: ['electric', 'yellow']
+            },
 
-                {
-                    name: 'Pikachu', 
-                    height: 0.4, 
-                    types: ['electric', 'yellow']
-                },
+            {
+                name: 'Seel', 
+                height: 1.1, 
+                types: ['water', 'wet']
+            }
+        ]
 
-                {
-                    name: 'Seel', 
-                    height: 1.1, 
-                    types: ['water', 'wet']
-                }
-            ]
+    function getAll() {
+        return pokemonList;
+    }
 
-        function getAll() {
-            return pokemonList;
-        }
+    function add(pokemon) {
+        pokemonList.push(pokemon);
+    }
 
-        function add(pokemon) {
-            pokemonList.push(pokemon);
-        }
+    function addListItem(pokemon) {
+        let someList= document.querySelector('.pokemon-list');
+        let listItem= document.createElement('li');
+        let button= document.createElement('button');
+        button.innerText= pokemon.name;
+        button.classList.add('poke-button');
+        listItem.appendChild(button);
+        someList.appendChild(listItem);
+        button.addEventListener('click', function (event) {
+            showDetails(pokemon)
+        });
+    }        
 
-        function addListItem(pokemon) {
-            let someList= document.querySelector('.pokemon-list');
-            let listItem= document.createElement('li');
-            let button= document.createElement('button');
-            button.innerText= pokemon.name;
-            button.classList.add('poke-button');
-            listItem.appendChild(button);
-            someList.appendChild(listItem);
-            button.addEventListener('click', function (event) {
-                showDetails(pokemon)
-            });
-        }        
+    function showDetails(pokemon) {
+        console.log(pokemon);
+    }
 
-        function showDetails(pokemon) {
-            console.log(pokemon);
-        }
-
-        return {
-            getAll: getAll,
-            add: add,
-            addListItem: addListItem,
-        };
+    return {
+        getAll: getAll,
+        add: add,
+        addListItem: addListItem,
+    };
 
 })()
 
